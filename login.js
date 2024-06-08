e36468676a4538b6028330431ac6bb50d4dfa56b
IFTC-XLKJ <iftcceo@139.com>
2024-06-08 15:42:54 +0800

    IFTC

---
+++ login.js
@@ -0,0 +1,30 @@
+var document = this.document;
+var window = this.window;
+window.onload = function () {
+    var Slogin = document.getElementById('Slogin');
+    console.log(Slogin);
+    Slogin.addEventListener('click', function () {
+        var url = "https://api.codemao.cn/tiger/v3/web/accounts/login";
+		var postdata = {
+			'pid': '65edCTyg',
+            'identity': document.getElementById('id').value,
+            'password': document.getElementById('pw').value
+		}
+        fetch(url, {
+    		method: 'POST',
+    		headers: {
+        		'Content-Type': 'application/json',
+    		},
+    		body: JSON.stringify(postdata),
+		})
+		.then(response => response.json())
+		.then(data => {
+    		document.write(JSON.stringify(data.user_info));
+			localStorage.setItem("nickname", id);
+			localStorage.setItem("password", pw);
+		})
+		.catch((error) => {
+    		document.write('Error:', error);
+		});
+    })
+}
