ea1cf429d0decb79afd3dbd7f0190b61cbd791ef
IFTC-XLKJ <iftcceo@139.com>
2024-06-09 12:53:56 +0800

    Update

--- Home/login.js
+++ Home/login.js
@@ -5,45 +5,21 @@ console.log(document);
 document.addEventListener('DOMContentLoaded', function () {
 	console.log("onload");
 	var Slogin = document.getElementById('Slogin');
+	var Blogin = document.getElementById('Blogin');
 	console.log(Slogin);
 	Slogin.addEventListener('click', function () {
-		var url = "https://api.codemao.cn/tiger/v3/web/accounts/login";
-		/*function handleResponse(data) {
-			document.getElementById('output').innerText = JSON.stringify(data);
-		}
-		function sendJSONPRequest(url, data) {
-			const callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-			window[callbackName] = function (response) {
-				delete window[callbackName];
-				document.body.removeChild(script);
-				handleResponse(response);
-			};
-			const requestUrl = url + '?callback=' + callbackName + '&data=' + encodeURIComponent(JSON.stringify(data));
-			const script = document.createElement('script');
-			script.src = requestUrl;
-			document.body.appendChild(script);
-		}
-
-		sendJSONPRequest(url, postData);*/
-		const socket = new WebSocket('https://api.codemao.cn/tiger/v3/web/accounts/login');
-		socket.onopen = function (event) {
-			const postData = {
-				'pid': '65edCTyg',
-				'identity': document.getElementById('id').value,
-				'password': document.getElementById('pw').value
-			};
-			const requestData = {
-				method: 'POST',
-				data: JSON.stringify(postData)
-			};
-			socket.send(JSON.stringify(requestData));
-		};
-		socket.onmessage = function (event) {
-			const responseData = JSON.parse(event.data);
-			console.log('Response from server:', responseData);
-		};
-		socket.onclose = function (event) {
-			console.log('Connection closed');
-		};
+		console.log("click");
+		
+	})
+	Blogin.addEventListener('click', function () {
+		console.log("click");
+		const qrcodeDiv = document.getElementById('qrlogin');
+		const text = `{"time":${Date.now()},"type":"login"}`;
+		console.log(JSON.parse(text));
+		new QRCode(qrcodeDiv, {
+			text: text,
+			width: 250,
+			height: 250,
+		});
 	})
 });
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var url = window.location.href;
-var params = new URLSearchParams(url.search);
-var paramsDict = {};
-params.forEach(function(value, key) {
-    paramsDict[key] = value;
-});
-
-document.addEventListener('DOMContentLoaded', function() {
+var window = this.window;
+document.addEventListener('DOMContentLoaded', function () {
+    var url = window.location.href;
+    var params = new URLSearchParams(url.search);
+    var paramsDict = {};
+    params.forEach(function (value, key) {
+        paramsDict[key] = value;
+    });
     console.log(paramsDict);
+    if (paramsDict['page']) {
+        window.location.href = `./${paramsDict['page']}`;
+    } else {
+        window.location.href = `./Home`;
+    }
 });
