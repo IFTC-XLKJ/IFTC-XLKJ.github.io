aace7743889f4c528ed96e2e53bdf8b4ff68f717
IFTC-XLKJ <iftcceo@139.com>
2024-06-09 15:15:36 +0800

    Update

--- Home/login.js
+++ Home/login.js
@@ -7,9 +7,14 @@ document.addEventListener('DOMContentLoaded', function () {
 	var Slogin = document.getElementById('Slogin');
 	var Blogin = document.getElementById('Blogin');
 	console.log(Slogin);
+	var weblogin = new pgdbs(dbs_29956f7ff3449ed31f3e59bb3eb685d93ed73e96f6f9dfe01f2f65cbb08dcc14);
+	console.log(weblogin);
 	Slogin.addEventListener('click', function () {
 		console.log("click");
-		
+		weblogin.onGetData((json, id ,url) =>{
+			alert(JSON.stringify(json));
+		});
+		weblogin.getTableData({page:1,limit:30,id:'getTableData'})
 	})
 	Blogin.addEventListener('click', function () {
 		console.log("click");
