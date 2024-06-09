6bd2fa425988c074b1dae79201c7af5461742758
IFTC-XLKJ <iftcceo@139.com>
2024-06-09 13:53:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var window = this.window;
 document.addEventListener('DOMContentLoaded', function () {
-    var url = window.location.href;
-    var params = new URLSearchParams(url.search);
-    var paramsDict = {};
-    params.forEach(function (value, key) {
-        paramsDict[key] = value;
-    });
-    console.log(paramsDict);
-    if (paramsDict['page']) {
-        window.location.href = `./${paramsDict['page']}`;
+    function getURLParameters() {
+        const queryString = window.location.search.substring(1); // 移除问号
+        const params = {};
+
+        if (queryString) {
+            queryString.split('&').forEach(param => {
+                const [key, value] = param.split('=');
+                params[key] = decodeURIComponent(value);
+            });
+        }
+
+        return params;
+    }
+
+    const urlParams = getURLParameters();
+    console.log(urlParams);;
+    if (urlParams['page']) {
+        console.log(urlParams['page']);
+        window.location.href = `./${urlParams['page']}`;
     } else {
         window.location.href = `./Home`;
     }
