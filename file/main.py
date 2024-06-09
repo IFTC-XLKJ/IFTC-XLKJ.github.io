0fbd40dec928be28bb5f87e3e374b6e98f220922
IFTC-XLKJ <iftcceo@139.com>
2024-06-09 17:43:12 +0800

    Create main.py

---
+++ file/main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+import json
+
+from vika import Vika
+vika = Vika("uskWED5bEniUSC8nyCmavZ3")
+#通过 datasheetId 来指定要从哪张维格表操作数据。
+datasheet = vika.datasheet("dstEB3X1XcovJko4WR", field_key="id")
+
+root = tk.Tk()
+root.withdraw()
+f_path = filedialog.askopenfilename()
+print('获取的文件地址：',f_path)
+print('上传中，请稍后')
+_file = datasheet.upload_file(f_path)
+print(_file['url'])
