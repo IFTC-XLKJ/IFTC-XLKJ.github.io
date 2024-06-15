1c5759f97fea477e3ef995dbbc5a6af5aff2d74a
IFTC-XLKJ <iftcceo@139.com>
2024-06-15 14:58:41 +0800

    Update

--- VVMusic/index.js
+++ VVMusic/index.js
@@ -1,3 +1,45 @@
 function SearchAPI(name, pagesize, page, n) {
     return `https://api.xingzhige.com/API/NetEase_CloudMusic_new/?name=${name}&pagesize=${pagesize}&page=${page}${n > 0 && 0 % 1 === 0 ? `&n=${n}` : ''}`
 }
+document.addEventListener('DOMContentLoaded', () => {
+    var search = document.querySelector('#s > button');
+    function getMusic() {
+        var keyword = document.querySelector('#s > input').value;
+        if(keyword){
+            console.log(keyword);
+            $.ajax({
+                url: SearchAPI(keyword, 10, 1),
+                type: 'GET',
+                data: {
+                    keyword: keyword
+                },
+                success: function(data) {
+                    var result = data.data;
+                    console.log(result);
+                    var music = document.getElementById('music');
+                    music.innerHTML = '';
+                    result.forEach(item => {
+                        music.innerHTML += `
+                        <div class="music-item">
+                            <div class="music-item-img">
+                                <img src="${item.cover}" alt="${item.songname}" title="${item.songname} - ${item.name}">
+                            </div>
+                        </div>
+                        `;
+                    });
+                },
+                error: function(err) {
+                    console.log(err);
+                }
+            });
+        }
+    }
+    search.addEventListener('click', () => {
+        getMusic();
+    })
+    document.querySelector('#s > input').addEventListener('keyup', (e) => {
+        if (e.keyCode == 13) {
+            getMusic();
+        }
+    })
+})
