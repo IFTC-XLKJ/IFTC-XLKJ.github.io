a4a91a5ce04878e5c88caff06c87f2192d01f85e
IFTC-XLKJ <iftcceo@139.com>
2024-06-15 19:38:10 +0800

    Update index.js

--- VVMusic/index.js
+++ VVMusic/index.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     var search = document.querySelector('#s > button');
     function getMusic() {
         var keyword = document.querySelector('#s > input').value;
-        if(keyword){
+        if (keyword) {
             console.log(keyword);
             $.ajax({
                 url: SearchAPI(keyword, 100, 1),
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 data: {
                     keyword: keyword
                 },
-                success: function(data) {
+                success: function (data) {
                     var result = data.data;
                     console.log('结果：', result);
                     var music = document.getElementById('music');
                     music.innerHTML = '';
-                    if(result.code){console.log(true);}
+                    if (result.code) { console.log(true); }
                     result.forEach(item => {
                         music.innerHTML += `
                         <div class="music-item">
@@ -36,9 +36,19 @@ document.addEventListener('DOMContentLoaded', () => {
                         `;
                     });
                     var page = docuemnt.getElementById('page');
-                    page.innerHTML = `<button class='page'></button>`;
+                    page.innerHTML = `
+                    <button class='page' id='previous'>上一页</button>
+                    <input type='number' id='page-input' value='${pageNum}'>
+                    <button class='page' id='next'>下一页</button>
+                    `;
+                    var musiclist = document.getElementsByClassName('music-item');
+                    musiclist.forEach(function (item) {
+                        item.addEventListener('click', function () {
+                            console.log(item);
+                        })
+                    })
                 },
-                error: function(err) {
+                error: function (err) {
                     var page = docuemnt.getElementById('page');
                     page.innerHTML = ``;
                     var music = document.getElementById('music');
@@ -57,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     var newWidth = window.innerWidth;
     console.log('窗口大小发生了变化！', `宽：${newWidth}px`);
 });
