b3d24f5b8e22977fc12418dcf212545a44b55f0a
IFTC-XLKJ <iftcceo@139.com>
2024-06-24 15:54:41 +0800

    Update data.js

--- VVMusic/data.js
+++ VVMusic/data.js
@@ -7,19 +7,11 @@ async function fetchIP() {
     try {
         const response = await fetch('https://api.ipify.org?format=json');
         const data = await response.json();
+        print(data);
         return data.ip;
     } catch (error) {
         console.error('Error fetching IP:', error);
         throw error;
-    }
-}
-async function displayIP() {
-    try {
-        const ip = await fetchIP();
-        console.log('User IP:', ip);
-
-    } catch (error) {
-        console.error('Failed to get IP:', error);
     }
 }
 
@@ -29,13 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
     var data = new pgdbs(dbs_3568fe5b83b522f9bbdf30a05205eef2d885d97956c78f565f2d7edb605f05eb);
     print(data);
     function addAccess() {
-        data.onGetData((json, id) => {
+        data.onGetData((json, id, url) => {
             if (id == '添加访问量数据') {
                 if (json.code == 200) {
                     times = 0;
                     print(json);
                 } else {
-                    if (times <= 3) {
+                    if (times < 3) {
                         times++;
                         print(`第${times}次重试ing...`);
                         addAccess();
@@ -44,25 +36,32 @@ document.addEventListener("DOMContentLoaded", function () {
                     }
                 }
             }
-
         });
         setTimeout(() => {
-            data.setTableData({
-                type: 'INSERT',
-                fields: `('/VVMusic','${async () => {
+            async function displayIP() {
+                try {
                     const ip = await fetchIP();
                     console.log('User IP:', ip);
-                    return ip;
-                }}')`,
-                filter: '站点,IP',
-                id: '添加访问量数据'
-            });
+                    data.setTableData({
+                        type: 'INSERT',
+                        fields: `('/VVMusic','${ip}')`,
+                        filter: '站点,IP',
+                        id: '添加访问量数据'
+                    });
+                } catch (error) {
+                    console.error('Failed to get IP:', error);
+                    throw error;
+                    addAccess();
+                }
+            }
+            print(['addAccess'])
+            displayIP();
         }, 200);
     }
     function getData() {
         data.onGetData((json, id, url) => {
             if (id == '获取访问量数据') {
-                print(['获取：', json])
+                print(['获取：', json, json.code == 200])
                 if (json.code == 200) {
                     times = 0;
                     print(json.fields);
