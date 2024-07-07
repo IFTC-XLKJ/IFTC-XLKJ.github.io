56c5f8c9f3ad38288d5f1c223b57eb3cdbc2a40c
IFTC-XLKJ <iftcceo@139.com>
2024-07-07 13:43:25 +0800

    Update index.js

--- player/video/index.js
+++ player/video/index.js
@@ -1,6 +1,22 @@
 var document = this.document;
 var window = this.window;
-document.addEventListener('DOMContentLoaded', function() {
+function getURLParameters() {
+    const queryString = window.location.search.substring(1);
+    const params = {};
+
+    if (queryString) {
+        queryString.split('&').forEach(param => {
+            const [key, value] = param.split('=');
+            params[key] = decodeURIComponent(value);
+        });
+    }
+
+    return params;
+}
+
+const urlParams = getURLParameters();
+console.log(urlParams);
+document.addEventListener('DOMContentLoaded', function () {
     var video = document.getElementById('video');
     var app = document.getElementById('app');
 });
