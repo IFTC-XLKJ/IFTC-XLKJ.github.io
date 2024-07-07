e5c57d2bf35916ae29a803f8df1bbb78ef2e6a65
IFTC-XLKJ <iftcceo@139.com>
2024-07-07 13:45:59 +0800

    更新

--- player/video/index.js
+++ player/video/index.js
@@ -3,17 +3,14 @@ var window = this.window;
 function getURLParameters() {
     const queryString = window.location.search.substring(1);
     const params = {};
-
     if (queryString) {
         queryString.split('&').forEach(param => {
             const [key, value] = param.split('=');
             params[key] = decodeURIComponent(value);
         });
     }
-
     return params;
 }
-
 const urlParams = getURLParameters();
 console.log(urlParams);
 document.addEventListener('DOMContentLoaded', function () {
