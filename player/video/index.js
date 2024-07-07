0e1f64c83da67eb7438e584809cfc114cdb4a9cd
IFTC-XLKJ <iftcceo@139.com>
2024-07-07 13:42:21 +0800

    Create index.js

---
+++ player/video/index.js
@@ -0,0 +1,6 @@
+var document = this.document;
+var window = this.window;
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('video');
+    var app = document.getElementById('app');
+});
