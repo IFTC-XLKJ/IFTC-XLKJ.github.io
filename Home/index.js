fcd90b51d0172d8c944fa5a1e6e394e1d5e69984
IFTC-XLKJ <iftcceo@139.com>
2024-07-10 12:17:17 +0800

    更新

--- Home/index.js
+++ Home/index.js
@@ -1,12 +1,16 @@
 var document = this.document;
 var window = this.window;
 var appnum = 1;
+var appsURL = [
+    'https://iftc-xlkj.github.io/VV图书118.apk'
+]
 document.addEventListener('DOMContentLoaded', function () {
     var vvzh = new pgdbs(dbs_a6b2a4d6c02022e831626d31ab805a468a151b90d5161660485a73cc6e1ea902);
     var user = document.getElementById('user');
     var Blogin = document.getElementById('Blogin');
     var userImg = document.getElementById('user_img');
     var login = document.getElementById('login');
+    var appItems = document.querySelectorAll('.app_item');
     if (localStorage.getItem('ID')) {
         user.style.display = 'inline';
         user.style.height = '60px';
@@ -15,4 +19,16 @@ document.addEventListener('DOMContentLoaded', function () {
         Blogin.style.display = 'none';
         userImg.src = localStorage.getItem('头像');
     }
+    console.log(appItems)
+    appItems.forEach(function (appItem, index) {
+        appItem.addEventListener('click', function () {
+            var a = document.createElement('a');
+            var url = appsURL[index];
+            a.href = url;
+            a.target = '_blank';
+            a.style.display = 'none';
+            a.appendChild(document.createTextNode('APK'));
+            a.click();
+        });
+    });
 });
