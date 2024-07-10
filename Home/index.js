9db394a69c872e0330705781f2dcef2e2fae35ea
IFTC-XLKJ <iftcceo@139.com>
2024-07-10 13:25:03 +0800

    更新

--- Home/index.js
+++ Home/index.js
@@ -2,7 +2,8 @@ var document = this.document;
 var window = this.window;
 var appnum = 1;
 var appsURL = [
-    'https://iftc-xlkj.github.io/apk/VV图书118.apk'
+    'https://iftc-xlkj.github.io/apk/VV图书118.apk',
+    ''
 ]
 document.addEventListener('DOMContentLoaded', function () {
     var vvzh = new pgdbs(dbs_a6b2a4d6c02022e831626d31ab805a468a151b90d5161660485a73cc6e1ea902);
