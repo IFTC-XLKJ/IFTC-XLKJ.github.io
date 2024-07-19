087e0f1bd9d156f99eb87cf4eec3c5dcaa16877e
IFTC-XLKJ <iftcceo@139.com>
2024-07-19 19:35:47 +0800

    Update share.js

--- VVMusic/share.js
+++ VVMusic/share.js
@@ -1,4 +1,18 @@
 var document = this.document;
 var window = this.window;
 docuemnt.addEventListener('DOMContentLoaded', function () {
+    var share = document.getElementById('share');
+    var Share = new pgdbs(dbs_09acf4d4695fb1a5f1f94b8dd8a0c877ed7099b0bccab1c904087a70445dc8b3);
+    function ShareID() {
+        const ShareID = '';
+        const Strings = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        for (let i = 0; i < 16; i++) {
+            ShareID += Strings[Math.floor(Math.random() * Strings.length)];
+        }
+        return ShareID;
+    }
+    share.addEventListener('click', function () {
+        loading.showModal();
+        console.log(ShareID());
+    })
 })
