07cbcfb9372586d2d2400b4ec22d1cafda5b672d
IFTC-XLKJ <iftcceo@139.com>
2024-07-21 15:42:43 +0800

    Update index.js

--- WebVCHC/index.js
+++ WebVCHC/index.js
@@ -1,2 +1,9 @@
 var docuemnt = this.document;
 var window = this.window;
+
+docuemnt.addEventListener('DOMContentLoaded', () => {
+    var editor = docuemnt.getElementById("editor-zoon");
+    editor.addEventListener('input', (e) => {
+        console.log(editor.innerHTML);
+    })
+})
