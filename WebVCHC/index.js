4aab00d1f11c34fc41e32be51b11e230e5122f04
IFTC-XLKJ <iftcceo@139.com>
2024-07-21 15:49:57 +0800

    Update index.js

--- WebVCHC/index.js
+++ WebVCHC/index.js
@@ -1,9 +1,18 @@
 var docuemnt = this.document;
 var window = this.window;
 
+var code = {
+    print: function (text) {
+        output.innerHTML += text;
+    },
+}
 docuemnt.addEventListener('DOMContentLoaded', () => {
     var editor = docuemnt.getElementById("editor-zoon");
-    editor.addEventListener('input', (e) => {
-        console.log(editor.innerHTML);
-    })
+    var output = docuemnt.getElementById("output-zoon");
+    var run = docuemnt.getElementById("run");
+    run.onclick = function () {
+        output.innerHTML = '';
+        var codes = JSON.parse(editor.innerHTML);
+        output.innerHTML += `程序名：${codes.程序名}`
+    }
 })
