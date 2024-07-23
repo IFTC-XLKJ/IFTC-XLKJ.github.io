30a663735f4321f53498a0db8dfc4b6af511108d
IFTC-XLKJ <iftcceo@139.com>
2024-07-23 16:38:59 +0800

    Update index.js

--- VCC/index.js
+++ VCC/index.js
@@ -28,11 +28,28 @@ document.addEventListener('DOMContentLoaded', function () {
         var p = ps[ps.length - 1];
         return main.offsetWidth - p.offsetWidth;
     }
-    main.innerHTML += `VCC [版本：${version}]<br>© IFTC 2020-2024 All Rights Reserved.<br><br><p class="user">IFTC://${murmur}></p>`
+    main.innerHTML += `VCC [版本：${version}]<br>© IFTC 2020-2024 All Rights Reserved.<br>`
     function inputer() {
+        main.innerHTML += `<br><p class="user">IFTC://${murmur}></p>`;
         main.innerHTML += `<input style="width: ${inputWidth() - 1}px;">`;
         var input = document.querySelector('input');
-        input.focus()
+        input.focus();
+        input.addEventListener('keydown', function (e) {
+            if (e.key == 'Enter') {
+                input.remove();
+                main.innerHTML += `${input.value}`;
+                var command = (input.value).split(' ');
+                console.log(command);
+                if ((command[0]).trim() == "") {
+                    inputer();
+                } else if (command[0] == "clear") {
+                    main.innerHTML = `VCC [版本：${version}]<br>© IFTC 2020-2024 All Rights Reserved.<br>`;
+                    inputer();
+                } else if (command[0] == "help") {
+                    main.innerHTML += `<br>&nbsp;clear：清空屏幕<br>&nbsp;help：显示帮助<br>`;
+                }
+            }
+        });
     }
     inputer();
 });
