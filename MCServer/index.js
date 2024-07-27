b79d2ed7f2410c7143dc16d61c88f00bc2212ac9
IFTC-XLKJ <iftcceo@139.com>
2024-07-27 08:21:16 +0800

    Update index.js

--- MCServer/index.js
+++ MCServer/index.js
@@ -2,19 +2,21 @@ var window = this.window;
 var document = this.document;
 
 document.addEventListener('DOMContentLoaded', function () {
-    var footer = document.getElementById('footer');
-    var winheight = window.innerHeight;
-    var banner = document.getElementById('banner');
-    var main = document.getElementById('main');
-    var bannerheight = banner.offsetHeight;
-    var mainheight = main.offsetHeight;
-    if (bannerheight + mainheight < winheight) {
-        var footerheight = footer.offsetHeight;
-        footer.style.position = 'fixed';
-        footer.style.top = `${winheight - footerheight}px`;
+    for (; ;) {
+        var footer = document.getElementById('footer');
+        var winheight = window.innerHeight;
+        var banner = document.getElementById('banner');
+        var main = document.getElementById('main');
+        var bannerheight = banner.offsetHeight;
+        var mainheight = main.offsetHeight;
+        if (bannerheight + mainheight < winheight) {
+            var footerheight = footer.offsetHeight;
+            footer.style.position = 'fixed';
+            footer.style.top = `${winheight - footerheight}px`;
+        }
     }
 });
-
+/*
 window.addEventListener('resize', function () {
     var footer = document.getElementById('footer');
     var winheight = window.innerHeight;
@@ -27,4 +29,4 @@ window.addEventListener('resize', function () {
         footer.style.position = 'fixed';
         footer.style.top = `${winheight - footerheight}px`;
     }
-})
+})*/
