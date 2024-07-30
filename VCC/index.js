df6bf7263ef5765190854b859bb0ff019d27e90a
IFTC-XLKJ <iftcceo@139.com>
2024-07-30 14:01:24 +0800

    Update index.js

--- VCC/index.js
+++ VCC/index.js
@@ -421,12 +421,40 @@ document.addEventListener('DOMContentLoaded', function () {
                             }
                         })
                     } else {
-                        main.innerHTML += `<br>正在查询""的IP地址...`;
+                        main.innerHTML += `<br>正在查询当前的IP地址...`;
                         $.ajax({
                             url: "https://api.ipify.org?format=json",
                             type: "get",
                             dataType: "json",
-                            success: function (data) { }
+                            success: function (data) {
+                                $.ajax({
+                                    url: "https://api.sageded.top/api/other/ip?ips=" + data.ip,
+                                    type: "get",
+                                    dataType: "json",
+                                    success: function (data) {
+                                        if (data.flag) {
+                                            main.innerHTML += `<br><div style="color: green;">查询成功</div>`;
+                                            (data.data).forEach((item, index) => {
+                                                main.innerHTML += `<br>第${index + 1}条IP地址（${item.ip}）<br>`;
+                                                main.innerHTML += `IP地址所属位置：${item.addr}<br>`;
+                                                main.innerHTML += `省份编码：${item.proCode}<br>`;
+                                                main.innerHTML += `城市编码：${item.cityCode}<br>`;
+                                            })
+                                        } else {
+                                            main.innerHTML += `<div style="color: red;">查询失败</div><br>`;
+                                        }
+                                        inputer();
+                                    },
+                                    error: function (err) {
+                                        main.innerHTML += `<div style="color: red;">查询失败</div><br>`;
+                                        inputer();
+                                    }
+                                })
+                            },
+                            error: function (err) {
+                                main.innerHTML += `<div style="color: red;">查询失败</div><br>`;
+                                inputer();
+                            }
                         })
                     }
                 }
